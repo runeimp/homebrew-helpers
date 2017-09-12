227672e188a4a301965fa408c2e648d0fb3572ea
Mark Gardner <mark.gardner@twobitcircus.com>
2017-09-12 15:01:17 -0700

    updated brew-url to v1.1.0 updated brewlist.sh to v0.2.0

--- brew-url.sh
+++ brew-url.sh
@@ -7,16 +7,66 @@
 #####
 # ChangeLog:
 # ----------
+# 2017-03-15  1.1.0      Added -c and -d options and smarter parsing. 
 # 2016-05-25  1.0.0      Initial script creation?
 #
 
-app="$1"
+browser='Google Chrome'
+declare -i caskroom=1
 
-case "${app:0:8}" in
-	[Cc]askroom)
-		open -a 'Google Chrome' $(brew cask info $app | grep -v caskroom | egrep ^https?:.*)
-		;;
-	*)
-		open -a 'Google Chrome' $(brew info $app | egrep ^https?:.*)
-		;;
-esac
+# echo "\$@: $@"
+
+until [[ $# -eq 0 ]]; do
+	case "$1" in
+		-c)
+			browser='Google Chrome'
+			;;
+		-d)
+			browser=Safari
+			;;
+		[Cc]askroom*)
+			caskroom=0
+			;;
+		*)
+			app="$1"
+			;;
+	esac
+
+	shift
+done
+
+# echo "browser: $browser"
+# echo "caskroom: $caskroom"
+# echo "app: $app"
+
+result=$(brew info "$app" 2>&1)
+exit_code=$?
+
+# echo "result: $result"
+# echo "exit_code: $exit_code"
+
+if [[ $exit_code -gt 0 ]]; then
+	result=$(brew cask info "$app" 2>&1)
+	exit_code=$?
+	# echo "result: $result"
+	# echo "exit_code: $exit_code"
+fi
+
+if [[ $exit_code -gt 0 ]]; then
+	echo "Couldn't find app in Homebrew" 1>&2
+else
+	url=$(echo "$result" | grep -v caskroom | egrep ^https?:.* )
+	open -a "$browser" "$url"
+fi
+
+
+# app="$1"
+
+# case "${app:0:8}" in
+# 	[Cc]askroom)
+# 		open -a 'Google Chrome' $(brew cask info $app | grep -v caskroom | egrep ^https?:.*)
+# 		;;
+# 	*)
+# 		open -a 'Google Chrome' $(brew info $app | egrep ^https?:.*)
+# 		;;
+# esac
--- brewlist.sh
+++ brewlist.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 ###################
-# brewlist v0.1.0
+# brewlist v0.2.0
 #
 # @author RuneImp <runeimp@gmail.com>
 # @licenses http://opensource.org/licenses/MIT
 #
 #
 
+APP_NAME="BrewList"
+APP_VERSION="0.2.0"
+APP_LABEL="$APP_NAME v$APP_VERSION"
+
 conf=""
-file="brew-list.md"
+file="brew-list"
+file_ext=".md"
 i=0
 list=""
 output=""
+prefix=""
 title="Brew List"
 title_length=0
 title_underline=""
@@ -25,13 +31,47 @@ title_length=${#title}
 # 	let "i += 1"
 # done
 
+until [[ $# -eq 0 ]]; do
+	case "$1" in
+		-e | --ext | --extension)
+			file_ext="$2"
+			shift
+			;;
+		-f | --file)
+			file="$2"
+			shift
+			;;
+		-p | --prefix)
+			prefix="$2"
+			shift
+			;;
+		-s | --suffix)
+			suffix="$2"
+			shift
+			;;
+		-V | --version)
+			echo "$APP_LABEL"
+			exit 0
+			;;
+		*)
+			echo "Unknown option '$1'" 1>&2
+			exit 1
+			;;
+	esac
+	shift
+done
+
+filename="${prefix}${file}${suffix}${file_ext}"
+
+# echo "\$filename: $filename"
+# exit 69
 
 while [[ ${#title_underline} < $title_length ]]; do
 	title_underline="${title_underline}="
 done
 
 
-for item in $(brew list); do
+for item in $(brew leaves); do
 	list=$(printf "${list}\n* %s" "$item")
 done
 
@@ -47,10 +87,10 @@ output="$(printf "%s\n%s\n%s\n\n\nBrew Config\n-----------\n\n\`\`\`%s\n\`\`\`\n
 
 # echo "$output"
 
-echo "$output" > "$file"
-if [[ $? = 0 ]]; then
-	echo "$file created successfully."
+echo "$output" > "$filename"
+if [[ $? -eq 0 ]]; then
+	echo "$filename created successfully."
 else
-	echo "Error writing $file to the system."
+	echo "Error writing $filename to the system."
 fi
 
